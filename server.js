c5984a98a7324812a32bcfcaefeb5e1e9cb35ed4
Arcade233 <alex1ander2444@gmail.com>
2026-09-11 20:01:44 +0000

    Serve static files and add index.html route
    Added static file serving and a route to serve index.html.

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: { origin: "*" }
+});
+
+// Serve static files (like index.html) from your repo directory
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 let roundPhase = 'WAITING';
